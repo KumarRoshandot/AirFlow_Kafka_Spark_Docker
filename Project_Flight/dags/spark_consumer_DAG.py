1ce07c8779be2cd22a0f0fbaa5741d4121db3816
Kumar Roshan <52309589+KumarRoshandot@users.noreply.github.com>
2020-01-15 01:14:29 +0530

    Update spark_consumer_DAG.py

--- Project_Flight/dags/spark_consumer_DAG.py
+++ Project_Flight/dags/spark_consumer_DAG.py
@@ -35,7 +35,7 @@ task2 = BashOperator(
 
 task3 = DockerOperator(
     task_id = 'pyspark_consumer',
-    image='incremental_training-master_spark:latest',
+    image='Project_Flight_spark:latest',
     api_version='auto',
     auto_remove=True,
     volumes=['/usr/local/airflow/dags/src/spark_consume_data:/spark_consume_data'],
